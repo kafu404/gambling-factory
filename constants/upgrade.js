56daff7e5cd6217774d15101091c39dac6e33273
Nguyen Huong Anh <huonganh.nguyen@ceff.ch>
2026-06-01 11:26:41 +0200

    début de la classe upgrade

---
+++ constants/upgrade.js
@@ -0,0 +1,14 @@
+class Upgrade{
+    constructor(price, mult) {
+        this.price = price;
+        this.mult = mult
+    }
+
+    // function pay(price, mult){
+    //     if( money >= price){
+    //         clearInterval(upgradeTimer)
+    //         upgradeTimer
+    //     }
+    // }
+
+}
